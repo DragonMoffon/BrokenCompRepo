92eabc201131adb2e26a8781b90cf942e65259b3
DragonMoffon <happydog.aja@gmail.com>
2021-06-30 21:38:28 +1200

    First Push. New Computer Who dis?

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ class MapWindow(arcade.Window):
     def on_key_release(self, symbol: int, modifiers: int):
         if symbol == arcade.key.ESCAPE or symbol == arcade.key.X:
             self.close()
+
+
+class RenderView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+
+
+RENDER_VIEW = RenderView()
+
+
+def main():
+    window = MapWindow()
+    window.show_view(RENDER_VIEW)
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
